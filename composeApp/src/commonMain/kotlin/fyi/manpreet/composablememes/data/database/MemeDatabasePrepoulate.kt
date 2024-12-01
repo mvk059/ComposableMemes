299a41aa725431e9ad371f92301b07209224b8e2
Manpreet Kunnath <manpreetkunnath@gmail.com>
2024-12-01 21:20:13 +0530

    Prepopulate all the images in the drawables to the database
    Prepopulate all images from commonMain/composeResources/drawable

--- composeApp/src/commonMain/kotlin/fyi/manpreet/composablememes/data/database/MemeDatabasePrepoulate.kt
+++ composeApp/src/commonMain/kotlin/fyi/manpreet/composablememes/data/database/MemeDatabasePrepoulate.kt
@@ -3,33 +3,30 @@ package fyi.manpreet.composablememes.data.database
 import androidx.room.RoomDatabase
 import androidx.sqlite.SQLiteConnection
 import androidx.sqlite.use
+import composablememes.composeapp.generated.resources.Res
+import composablememes.composeapp.generated.resources.allDrawableResources
 import fyi.manpreet.composablememes.di.DatabaseConstants
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
 import kotlinx.coroutines.launch
 import kotlinx.datetime.Clock
+import org.jetbrains.compose.resources.ExperimentalResourceApi
 
+@OptIn(ExperimentalResourceApi::class)
 fun prepopulateDatabase(): RoomDatabase.Callback {
     return object : RoomDatabase.Callback() {
         override fun onCreate(connection: SQLiteConnection) {
             super.onCreate(connection)
 
-            val memes = listOf(
-                MemeTable(
-                    imageUrl = "url1",
-                    createdDateInMillis = Clock.System.now().toEpochMilliseconds(),
-                    isFavorite = false
-                ),
-                MemeTable(
-                    imageUrl = "url2",
-                    createdDateInMillis = Clock.System.now().toEpochMilliseconds(),
-                    isFavorite = false
-                )
-            )
-
             CoroutineScope(Dispatchers.IO).launch {
-                memes.forEach { meme ->
+                val memes = Res.allDrawableResources.filter { it.key.endsWith("Meme") }
+                memes.forEach { (key, _) ->
+                    val meme = MemeTable(
+                        imageUrl = key,
+                        createdDateInMillis = Clock.System.now().toEpochMilliseconds(),
+                        isFavorite = false
+                    )
                     connection.prepare("INSERT INTO ${DatabaseConstants.TABLE_NAME} (imageUrl, createdDateInMillis, isFavorite) VALUES (?, ?, ?)")
                         .use { statement ->
                             statement.bindText(1, meme.imageUrl)

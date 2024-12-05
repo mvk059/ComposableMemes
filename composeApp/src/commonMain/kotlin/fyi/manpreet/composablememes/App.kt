898af555c5b1e90023aa824171ce967746d00c67
Manpreet Kunnath <manpreetkunnath@gmail.com>
2024-12-05 12:07:27 +0530

    [Migration] Update App class

--- composeApp/src/commonMain/kotlin/fyi/manpreet/composablememes/App.kt
+++ composeApp/src/commonMain/kotlin/fyi/manpreet/composablememes/App.kt
@@ -1,36 +1,67 @@
 package fyi.manpreet.composablememes
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
+import fyi.manpreet.composablememes.navigation.HomeDestination
+import fyi.manpreet.composablememes.navigation.MemeDestination
+import fyi.manpreet.composablememes.ui.home.HomeViewModel
+import fyi.manpreet.composablememes.ui.home.HomeScreen
+import fyi.manpreet.composablememes.ui.meme.MemeScreen
+import fyi.manpreet.composablememes.ui.theme.MemeTheme
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import composablememes.composeapp.generated.resources.Res
-import composablememes.composeapp.generated.resources.compose_multiplatform
+import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
 @Preview
-fun App() {
-    MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
+fun App(
+    viewModel: HomeViewModel = koinViewModel(),
+    navController: NavHostController = rememberNavController(),
+) {
+    val homeState = viewModel.homeState.collectAsStateWithLifecycle()
+    val allMemes = viewModel.allMemesList.collectAsStateWithLifecycle()
+
+    MemeTheme {
+
+        NavHost(
+            navController = navController,
+            startDestination = HomeDestination,
+        ) {
+
+            composable<HomeDestination> {
+
+                HomeScreen(
+                    homeState = homeState.value,
+                    memeListBottomSheet = allMemes.value,
+                    onFabClick = viewModel::onEvent,
+                    toggleSearchModeBottomSheet = viewModel::onEvent,
+                    onSearchTextChangeBottomSheet = viewModel::onEvent,
+                    onMemeSelectBottomSheet = {
+                        viewModel.onEvent(it)
+                        navController.navigate(MemeDestination(it.meme.imageName))
+                    },
+                    onFavoriteClick = viewModel::onEvent,
+                    onSelectClick = viewModel::onEvent,
+                    onEnterSelectionMode = viewModel::onEvent,
+                    onSelectedSortType = viewModel::onEvent,
+                    onCancelClickTopBar = viewModel::onEvent,
+                    onShareClickTopBar = viewModel::onEvent,
+                    onDeleteClickTopBar = viewModel::onEvent,
+                    onCancelClickDialog = viewModel::onEvent,
+                    onDeleteClickDialog = viewModel::onEvent,
+                )
             }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
+
+            composable<MemeDestination> {
+                val args = it.toRoute<MemeDestination>()
+                MemeScreen(
+                    memeName = args.memeName,
+                    navigateBack = { navController.navigateUp() }
+                )
             }
         }
     }

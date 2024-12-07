eb419678bf3000acd1691b1c7695793bddfff3ea
Manpreet Kunnath <manpreetkunnath@gmail.com>
2024-12-07 09:14:06 +0530

    Update min font size

--- composeApp/src/commonMain/kotlin/fyi/manpreet/composablememes/util/MemeConstants.kt
+++ composeApp/src/commonMain/kotlin/fyi/manpreet/composablememes/util/MemeConstants.kt
@@ -3,7 +3,7 @@ package fyi.manpreet.composablememes.util
 import androidx.compose.ui.unit.sp
 
 object MemeConstants {
-    val MIN_FONT_SIZE = 16.sp.value
+    val MIN_FONT_SIZE = 10.sp.value
     val MAX_FONT_SIZE = 32.sp.value
     const val DEFAULT_SLIDER_VALUE = 0.5f
 }

9f185eda512184352d4cc58c0a8c92b29518b49f
Manpreet Kunnath <manpreetkunnath@gmail.com>
2024-12-14 06:27:23 +0530

    Add value classes for ImageName  & ImagePath

--- composeApp/src/commonMain/kotlin/fyi/manpreet/composablememes/data/model/Meme.kt
+++ composeApp/src/commonMain/kotlin/fyi/manpreet/composablememes/data/model/Meme.kt
@@ -1,18 +1,17 @@
 package fyi.manpreet.composablememes.data.model
 
-import kotlinx.serialization.Serializable
+import kotlin.jvm.JvmInline
 
-@Serializable
 data class Meme(
     val id: Long = INVALID_ID,
-    val imageName: String,
-    val path: String? = null,
+    val imageName: MemeImageName,
+    val path: MemeImagePath? = null,
     val isFavorite: Boolean = false,
     val isSelected: Boolean = false,
 ) {
 
     fun checkImageNameCombinations(searchValue: String): Boolean {
-        val name = imageName.substring(0, imageName.lastIndexOf("_"))
+        val name = imageName.value.substring(0, imageName.value.lastIndexOf("_"))
         return name.contains(searchValue, ignoreCase = true)
     }
 
@@ -20,3 +19,9 @@ data class Meme(
         const val INVALID_ID = 0L
     }
 }
+
+@JvmInline
+value class MemeImageName(val value: String)
+
+@JvmInline
+value class MemeImagePath(val value: String)

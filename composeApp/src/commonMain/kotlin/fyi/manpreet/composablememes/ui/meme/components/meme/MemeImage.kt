a7a2e0f756622b4ac86a7ef59a10aabb80fd2667
Manpreet Kunnath <manpreetkunnath@gmail.com>
2024-12-16 07:23:58 +0530

    Remove BasicTextField border taking the entire height of the screen issue

--- composeApp/src/commonMain/kotlin/fyi/manpreet/composablememes/ui/meme/components/meme/MemeImage.kt
+++ composeApp/src/commonMain/kotlin/fyi/manpreet/composablememes/ui/meme/components/meme/MemeImage.kt
@@ -6,7 +6,14 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
@@ -15,7 +22,12 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawWithCache
@@ -266,8 +278,7 @@ private fun Content(
             modifier = Modifier
                 .width(IntrinsicSize.Min)
                 .height(IntrinsicSize.Min)
-                .padding(MaterialTheme.spacing.extraSmall)
-                .imePadding(),
+                .padding(MaterialTheme.spacing.extraSmall),
             enabled = false,
             readOnly = true,
             textStyle = TextStyle.Default.copy(
@@ -289,7 +300,6 @@ private fun Content(
                 .width(IntrinsicSize.Min)
                 .height(IntrinsicSize.Min)
                 .padding(MaterialTheme.spacing.extraSmall)
-                .imePadding()
                 .focusRequester(focusRequester),
             enabled = textBox.isEditable,
             textStyle = textBox.textStyle.copy(

7dac50625ea2798f7317a4bdb50441b4a1818978
Manpreet Kunnath <manpreetkunnath@gmail.com>
2024-12-18 21:59:47 +0530

    Add DI for network layer

--- composeApp/src/commonMain/kotlin/fyi/manpreet/composablememes/di/KoinModules.kt
+++ composeApp/src/commonMain/kotlin/fyi/manpreet/composablememes/di/KoinModules.kt
@@ -2,6 +2,8 @@ package fyi.manpreet.composablememes.di
 
 import fyi.manpreet.composablememes.data.datasource.MemeLocalDataSource
 import fyi.manpreet.composablememes.data.datasource.MemeLocalDataSourceImpl
+import fyi.manpreet.composablememes.data.datasource.MemeRemoteDataSource
+import fyi.manpreet.composablememes.data.datasource.MemeRemoteDataSourceImpl
 import fyi.manpreet.composablememes.data.repository.MemeRepository
 import fyi.manpreet.composablememes.data.repository.MemeRepositoryImpl
 import fyi.manpreet.composablememes.ui.home.HomeViewModel
@@ -14,15 +16,24 @@ import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.KoinAppDeclaration
 import org.koin.dsl.bind
 import org.koin.dsl.module
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.DEFAULT
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
 
 fun initKoin(config: KoinAppDeclaration? = null) =
     startKoin {
         config?.invoke(this)
         modules(
-            provideLocalDataSourceModule,
+            provideDataSourceModule,
             provideRepositoryModule,
             provideViewModelModule,
             provideUseCaseModule,
+            provideNetworkModule,
             provideDatabaseModule(),
             provideFileManagerModule(),
         )
@@ -33,8 +44,9 @@ val provideViewModelModule = module {
     viewModelOf(::MemeViewModel)
 }
 
-val provideLocalDataSourceModule = module {
+val provideDataSourceModule = module {
     singleOf(::MemeLocalDataSourceImpl).bind(MemeLocalDataSource::class)
+    singleOf(::MemeRemoteDataSourceImpl).bind(MemeRemoteDataSource::class)
 }
 
 val provideRepositoryModule = module {
@@ -43,4 +55,23 @@ val provideRepositoryModule = module {
 
 val provideUseCaseModule = module {
     factoryOf(::SaveImageUseCase)
+}
+
+val provideNetworkModule = module {
+    single { createJson() }
+    single { createHttpClient(get(), true) }
+}
+
+fun createJson() = Json { isLenient = true; ignoreUnknownKeys = true }
+
+fun createHttpClient(json: Json, enableNetworkLogs: Boolean) = HttpClient {
+    install(ContentNegotiation) {
+        json(json)
+    }
+    if (enableNetworkLogs) {
+        install(Logging) {
+            logger = Logger.DEFAULT
+            level = LogLevel.NONE
+        }
+    }
 }

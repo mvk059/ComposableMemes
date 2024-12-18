9520399df86e16f900b27407309552d446ad4f26
Manpreet Kunnath <manpreetkunnath@gmail.com>
2024-12-18 22:05:26 +0530

    Add Loader

---
+++ composeApp/src/commonMain/kotlin/fyi/manpreet/composablememes/ui/home/components/loader/Loader.kt
@@ -0,0 +1,59 @@
+package fyi.manpreet.composablememes.ui.home.components.loader
+
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.BrushPainter
+import androidx.compose.ui.graphics.painter.Painter
+import fyi.manpreet.composablememes.ui.theme.fixedAccentColors
+import fyi.manpreet.composablememes.ui.theme.spacing
+
+@Composable
+fun Loader() {
+    CircularProgressIndicator(
+        modifier = Modifier
+            .size(MaterialTheme.spacing.loaderSize)
+            .padding(vertical = MaterialTheme.spacing.medium),
+        color = MaterialTheme.fixedAccentColors.secondaryFixedDim
+    )
+}
+
+@Composable
+fun ShimmerBrush(): Painter {
+    val shimmerColors = listOf(
+        Color.LightGray.copy(alpha = 0.6f),
+        Color.LightGray.copy(alpha = 0.2f),
+        Color.LightGray.copy(alpha = 0.6f),
+    )
+
+    val transition = rememberInfiniteTransition(label = "")
+    val translateAnim = transition.animateFloat(
+        initialValue = 0f,
+        targetValue = 1000f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(1000, easing = LinearEasing),
+            repeatMode = RepeatMode.Restart
+        ),
+        label = ""
+    )
+
+    val brush = Brush.linearGradient(
+        colors = shimmerColors,
+        start = Offset.Zero,
+        end = Offset(x = translateAnim.value, y = translateAnim.value)
+    )
+
+    return BrushPainter(brush)
+}

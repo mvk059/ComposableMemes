88b6c471517d7d0da438ba1b46f2b418559bd83c
Manpreet Kunnath <manpreetkunnath@gmail.com>
2024-12-19 11:08:26 +0530

    Remove Room files

--- composeApp/src/androidMain/kotlin/fyi/manpreet/composablememes/di/DatabaseModule.android.kt
+++
@@ -1,9 +0,0 @@
-package fyi.manpreet.composablememes.di
-
-import fyi.manpreet.composablememes.data.database.MemeDatabase
-import fyi.manpreet.composablememes.database.getMemeDatabase
-import org.koin.dsl.module
-
-actual fun provideDatabaseModule() = module {
-    single<MemeDatabase> { getMemeDatabase(get()) }
-}

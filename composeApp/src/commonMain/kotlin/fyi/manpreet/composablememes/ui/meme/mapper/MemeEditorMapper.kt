f388aa137fb558c0d0744fe1ddb78eadf3856df4
Manpreet Kunnath <manpreetkunnath@gmail.com>
2024-12-22 13:16:26 +0530

    Update max & min font size based on platforms

--- composeApp/src/commonMain/kotlin/fyi/manpreet/composablememes/ui/meme/mapper/MemeEditorMapper.kt
+++ composeApp/src/commonMain/kotlin/fyi/manpreet/composablememes/ui/meme/mapper/MemeEditorMapper.kt
@@ -6,16 +6,19 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.sp
+import fyi.manpreet.composablememes.platform.platform.Platform
 import fyi.manpreet.composablememes.ui.meme.state.FontFamilyType
 import fyi.manpreet.composablememes.ui.theme.typographyFonts
 import fyi.manpreet.composablememes.util.MemeConstants
 
 typealias SliderValue = Float
 
-//@Composable
-fun SliderValue.sliderValueToFontSize(): TextUnit {
-    val minFontSize = MemeConstants.MIN_FONT_SIZE
-    val maxFontSize = MemeConstants.MAX_FONT_SIZE
+fun SliderValue.sliderValueToFontSize(platform: Platform): TextUnit {
+
+    val (minFontSize, maxFontSize) = when(platform) {
+        Platform.Android, Platform.Ios -> MemeConstants.MIN_FONT_SIZE_SMALL_SCREEN to MemeConstants.MAX_FONT_SIZE_SMALL_SCREEN
+        Platform.WasmJs -> MemeConstants.MIN_FONT_SIZE_LARGE_SCREEN to MemeConstants.MAX_FONT_SIZE_LARGE_SCREEN
+    }
 
     // Linear interpolation
     val mappedValue = minFontSize + (maxFontSize - minFontSize) * this
--- composeApp/src/commonMain/kotlin/fyi/manpreet/composablememes/util/MemeConstants.kt
+++ composeApp/src/commonMain/kotlin/fyi/manpreet/composablememes/util/MemeConstants.kt
@@ -3,8 +3,10 @@ package fyi.manpreet.composablememes.util
 import androidx.compose.ui.unit.sp
 
 object MemeConstants {
-    val MIN_FONT_SIZE = 10.sp.value
-    val MAX_FONT_SIZE = 32.sp.value
+    val MIN_FONT_SIZE_SMALL_SCREEN = 10.sp.value
+    val MAX_FONT_SIZE_SMALL_SCREEN = 32.sp.value
+    val MIN_FONT_SIZE_LARGE_SCREEN = 16.sp.value
+    val MAX_FONT_SIZE_LARGE_SCREEN = 100.sp.value
     const val DEFAULT_SLIDER_VALUE = 0.5f
     const val NAVIGATE_BACK_RELOAD = "Reload"
     const val TEMP_FILE_NAME = "temp_"
